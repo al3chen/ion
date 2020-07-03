59491b1cfc00bea7296ff439635eed941603d6b8
al3chen <al3chen@hotmail.com>
2020-07-04 00:16:07 +0800

    local config for my desktop

--- scripts/windows/bin/startup.bat
+++ scripts/windows/bin/startup.bat
@@ -2,8 +2,8 @@
 cls
 echo "start all services"
 start "redis log" "cmd /k .\Redis-x64-3.0.504\redis-server.exe"
-start "etcd log" "cmd /k .\etcd-v3.4.9-windows-amd64\etcd.exe --listen-client-urls http://0.0.0.0:2389 --advertise-client-urls http://0.0.0.0:2389"
-start "nats log" "cmd /k .\nats-server-v2.1.7-windows-amd64\nats-server.exe"
+start "etcd log" "cmd /k .\etcd-v3.4.9-windows-amd64\etcd.exe --listen-client-urls http://192.168.1.97:2389 --advertise-client-urls http://192.168.1.97:2389"
+start "nats log" "cmd /k .\nats-server-v2.1.7-windows-amd64\nats-server.exe -a 192.168.1.97"
 start "islb log" "cmd /k .\islb.exe -c islb.toml"
 start "biz log" "cmd /k .\biz.exe -c biz.toml"
 start "avp log" "cmd /k .\avp.exe -c avp.toml"
